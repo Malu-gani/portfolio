6f9e1b31a0ae72da5c834c31225450881ec75301
Malu-gani <maluganijuanmanuel@gmail.com>
2023-11-21 20:21:25 -0300

    hero section terminada

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,32 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import BackgroundCircles from "../components/BackgroundCircles";
+import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Juani portfolio</title>
-       
       </Head>
-      
-    <Header /> 
-    {/* Hero */}
-    <section id="hero" className="snap-center">
-       <Hero />
+      <Header />
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-    </section>
-    {/* About */}
-    {/* Experience */}
-    {/* Skills */}
-    {/* Projects */}
-    {/* Contact Me */}
       
+      {/* Experience */}
+
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
     </div>
   );
 };
