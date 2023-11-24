04063e0c56cd2fa8a6d5c780d55a243831e1593a
MaxiDumont <maximiliano.dumont@gmail.com>
2023-11-24 01:02:09 +0100

    experiencia y skills

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {};
 
@@ -20,18 +21,35 @@ export default function Hero({}: Props) {
       <BackgroundCircles />
       <img
         className="relative rounded-full h-32 w-32 mx-auto object-cover "
-        src="https://lh3.googleusercontent.com/a/ACg8ocKW80Zq2od3D_q1b5TcrrRGkpcvYQ1P7ajvH3Hx5qCvTCM=s288-c-no"
+        src="https://lh3.googleusercontent.com/a/ACg8ocKSRhIYOeFXPnAzyYfevofH8-x9K00Wt-XSRczKicqYikI=s360-c-no"
         alt=""
       />
-      <div>
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] ">
+      <div className="z-30">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px] ">
           Software Enginer
         </h2>
-        <h1 className="text-5xl lg:text-6xl front-semibold px-10">
+        <h1 className="text-1xl lg:text-4xl front-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
+
+        <div className="pt-5 ">
+          <Link href="@about">
+          <button className="heroButton">  About</button>
+          </Link>
+          <Link href="@experience">
+          <button className="heroButton">  Experience</button>
+          </Link>
+          <Link href="@skills">
+          <button className="heroButton">  Skills</button>
+          </Link>
+          <Link href="@porjects">
+          <button className="heroButton">  Porjects</button>
+          </Link>
+        </div>
       </div>
+
+
     </div>
   );
 }
